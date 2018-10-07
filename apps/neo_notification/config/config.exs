4de0b842ca633657dc8e868daac1bc31864d9865
Maksim Strenk <ephdtrg@gmail.com>
2018-10-07 14:17:55 +0200

    change configs to testnet

--- apps/neo_notification/config/config.exs
+++ apps/neo_notification/config/config.exs
@@ -1,3 +1,3 @@
 use Mix.Config
 
-config :neo_notification, notification_url_test: "http://notifications1.neeeo.org/v1"
+config :neo_notification, notification_url_test: "http://127.0.0.1:4020/v1"
--- apps/neoscan_node/config/config.exs
+++ apps/neoscan_node/config/config.exs
@@ -3,65 +3,37 @@ use Mix.Config
 if Mix.env() == :test do
   config :neoscan_node,
     notification_seeds: [
-      "http://notifications1.neeeo.org/v1"
+      "http://127.0.0.1:4020/v1"
     ]
 else
   config :neoscan_node,
     notification_seeds: [
-      "http://notifications1.neeeo.org/v1",
-      "http://notifications2.neeeo.org/v1",
-      "http://notifications3.neeeo.org/v1",
-      "http://notifications4.neeeo.org/v1"
+      "http://127.0.0.1:4020/v1"
     ]
 end
 
 config :neoscan_node,
   node_list_url:
-    "https://raw.githubusercontent.com/CityOfZion/neo-mon/master/docs/assets/mainnet.json"
+    "https://raw.githubusercontent.com/MyWishPlatform/neo-mon/mywish-testnet/docs/assets/testnet.json"
 
 config :neoscan_node,
   seeds: [
-    "https://seed1.switcheo.network:10331",
-    "https://seed3.switcheo.network:10331",
-    "http://seed1.travala.com:10332",
-    "https://seed1.neo.org:10331",
-    "https://seed1.cityofzion.io:443",
-    "https://seed2.cityofzion.io:443",
-    "https://seed3.cityofzion.io:443",
-    "https://seed4.cityofzion.io:443",
-    "https://seed5.cityofzion.io:443",
-    "https://seed0.cityofzion.io:443",
-    "https://seed6.cityofzion.io:443",
-    "https://seed7.cityofzion.io:443",
-    "https://seed8.cityofzion.io:443",
-    "https://seed9.cityofzion.io:443",
-    "https://seed1.redpulse.com:443",
-    "https://seed2.redpulse.com:443",
-    "https://seed.o3node.org:10331",
-    "http://seed1.aphelion-neo.com:10332",
-    "http://seed2.aphelion-neo.com:10332",
-    "http://seed4.aphelion-neo.com:10332",
-    "https://seed1.spotcoin.com:10332",
-    "http://rustylogic.ddns.net:10332",
-    "http://seed1.ngd.network:10332",
-    "http://seed2.ngd.network:10332",
-    "http://seed3.ngd.network:10332",
-    "http://seed4.ngd.network:10332",
-    "http://seed5.ngd.network:10332",
-    "http://seed6.ngd.network:10332",
-    "http://seed7.ngd.network:10332",
-    "http://seed8.ngd.network:10332",
-    "http://seed9.ngd.network:10332",
-    "http://seed10.ngd.network:10332"
+    "http://127.0.0.1:30330",
+    "http://127.0.0.1:30332",
+    "http://127.0.0.1:30333",
+    "http://127.0.0.1:30334",
+    "http://127.0.0.1:30335",
+    "http://127.0.0.1:30337"
   ]
 
 if Mix.env() == :test do
   config :neoscan_node,
     seeds: [
-      "https://seed1.cityofzion.io",
-      "https://seed2.cityofzion.io",
-      "https://seed3.cityofzion.io",
-      "https://seed4.cityofzion.io",
-      "https://seed5.cityofzion.io"
+      "http://127.0.0.1:30330",
+      "http://127.0.0.1:30332",
+      "http://127.0.0.1:30333",
+      "http://127.0.0.1:30334",
+      "http://127.0.0.1:30335",
+      "http://127.0.0.1:30337"
     ]
 end
